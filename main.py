ab7c4ad55eb023520c6df4caf0b87aca029f5872
Zack Here <muawyasolangi@gmail.com>
2024-12-16 16:42:43 +0500

    main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def send_message():
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background-color: white;
+            background-color: black;
             color: black;
         }
         .container {
